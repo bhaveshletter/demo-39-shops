af9580f64363d4955850c46c97d1a9af678a5527
bhaveshletter <bhaveshletter@gmail.com>
2020-08-30 23:08:17 +0530

    repeated moved to partial

--- app/assets/javascripts/editors.js
+++ app/assets/javascripts/editors.js
@@ -1,18 +1,152 @@
+// START background color
 function backgroundColor(e, component){
 	document.getElementById(component).style.backgroundColor = e.value;
 }
+// END background color
 
-function textColor(e, component){
-	document.getElementById(component).style.color = e.value;
+// START text color
+function textColor(e, component, tag){
+	var colorValue = e.value;
+	if(tag){	// TODO: find better way
+		var elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+			elements[i].style.color = colorValue;
+		}
+	} else {
+		document.getElementById(component).style.color = colorValue;
+	}
 }
+// END text color
 
+// START text size
 function fontSize(e, component){
-	document.getElementById(component).style.fontSize = e.options[e.selectedIndex].value.toString() + 'px';
+	document.getElementById(component).style.fontSize = e.options[e.selectedIndex].value;
+}
+// END text size
+
+// START font family
+function fontFamily(e, component, tag){
+	var fontValue = e.options[e.selectedIndex].value;
+	if(tag){	// TODO: find better way
+		var elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+			elements[i].style.fontFamily = fontValue;
+		}
+	} else {
+		document.getElementById(component).style.fontFamily = fontValue
+	}
+}
+// END font family
+
+// START font styling
+function fontBold(e, component, tag){
+	let cssStyle = 700;
+
+	if(tag){	// TODO: find better way
+		let elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+    		var element = elements[i].style;
+    		if(element.fontWeight == cssStyle){
+				element.fontWeight = '';
+    		}else{
+				element.fontWeight = cssStyle;
+    		}
+		}
+	} else {
+		let element = document.getElementById(component).style;
+		if(element.fontWeight == cssStyle){
+			element.fontWeight = '';
+		} else {
+			element.fontWeight = cssStyle
+		}
+	}
+
+	resetSelected(e);
 }
 
-function fontFamily(e, component){
-	document.getElementById(component).style.fontFamily = e.options[e.selectedIndex].value;
+function fontItalic(e, component, tag){
+	let cssStyle = 'italic';
+
+	if(tag){	// TODO: find better way
+		let elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+    		var element = elements[i].style;
+    		if(element.fontStyle == cssStyle){
+				element.fontStyle = '';
+    		}else{
+				element.fontStyle = cssStyle;
+    		}
+		}
+	} else {
+		let element = document.getElementById(component).style;
+		if(element.fontStyle == cssStyle){
+			element.fontStyle = '';
+		} else {
+			element.fontStyle = cssStyle
+		}
+	}
+
+	resetSelected(e);
 }
+
+function fontSmallCaps(e, component, tag){
+	let cssStyle = 'small-caps';
+
+	if(tag){	// TODO: find better way
+		let elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+    		var element = elements[i].style;
+    		if(element.fontVariant == cssStyle){
+				element.fontVariant = '';
+    		}else{
+				element.fontVariant = cssStyle;
+    		}
+		}
+	} else {
+		let element = document.getElementById(component).style;
+		if(element.fontVariant == cssStyle){
+			element.fontVariant = '';
+		} else {
+			element.fontVariant = cssStyle
+		}
+	}
+	resetSelected(e);
+}
+
+function fontUnderline(e, component, tag){
+	let cssStyle = 'underline';
+
+	if(tag){	// TODO: find better way
+		let elements = document.getElementById(component).getElementsByTagName(tag);
+    	for (let i = 0; i < elements.length; i++) {
+    		var element = elements[i].style;
+    		if(element.textDecoration == cssStyle){
+				element.textDecoration = '';
+    		}else{
+				element.textDecoration = cssStyle;
+    		}
+		}
+	} else {
+		let element = document.getElementById(component).style;
+		if(element.textDecoration == cssStyle){
+			element.textDecoration = '';
+		} else {
+			element.textDecoration = cssStyle
+		}
+	}
+
+	resetSelected(e);
+}
+
+function resetSelected(e){
+	let cssCls = 'selected';
+  	if (e.classList.contains(cssCls)) {
+    	e.classList.remove(cssCls);
+  	} else {
+		e.classList.add(cssCls);
+  	}
+}
+// END font styling
 
 function previewImage(e, control, component)	{
  	var ctrlImg = document.getElementById(control),
